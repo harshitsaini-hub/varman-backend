6dd05a4d35e42735c9c06ef31c8091daae8c5750
harshit saini <harshitsainidkr@gmail.com>
2026-05-05 17:35:41 +0530

    phase 1 and 2 backend completed

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
     return {"message": "AMOR backend running"}
 
-@app.post("/api/test")
-def test_endpoint(data: dict):
-    print("Received:", data)
-    return {"status": "received", "data": data}
+@app.post("/api/upload")
+async def upload_file(file: UploadFile = File(...)):
+    print("📸 File received:", file.filename, flush=True)
+
+    contents = await file.read()
+    print("File size:", len(contents), flush=True)
+
+    return {"filename": file.filename, "size": len(contents)}
