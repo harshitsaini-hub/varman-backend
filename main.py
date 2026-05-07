bce4c1b2fa65514cc26946f1384d6aa197135b42
harshit saini <harshitsainidkr@gmail.com>
2026-05-07 17:29:42 +0530

    implemented 2015 Caffe model for face detection (for testing and learning purpose)

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse, Response
-import os
 import cv2
 import numpy as np
+import os
+
+app = FastAPI()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,19 +13,21 @@ net = cv2.dnn.readNetFromCaffe(
     os.path.join(BASE_DIR, "models", "res10_300x300_ssd_iter_140000.caffemodel")
 )
 
-app = FastAPI()
-
 @app.post("/detect-face")
 async def detect_face(file: UploadFile = File(...)):
     try:
-        # Read image
         contents = await file.read()
+
         np_arr = np.frombuffer(contents, np.uint8)
+
         img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
         if img is None:
-            return JSONResponse(status_code=400, content={"error": "Invalid image format"})
-   
+            return JSONResponse(
+                status_code=400,
+                content={"error": "Invalid image"}
+            )
+
         (h, w) = img.shape[:2]
 
         blob = cv2.dnn.blobFromImage(
@@ -34,13 +38,17 @@ async def detect_face(file: UploadFile = File(...)):
         )
 
         net.setInput(blob)
+
         detections = net.forward()
 
         for i in range(detections.shape[2]):
+
             confidence = detections[0, 0, i, 2]
 
             if confidence > 0.6:
+
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+
                 (x1, y1, x2, y2) = box.astype("int")
 
                 cv2.rectangle(
@@ -50,9 +58,16 @@ async def detect_face(file: UploadFile = File(...)):
                     (0, 255, 0),
                     2
                 )
-        
+
         _, buffer = cv2.imencode(".jpg", img)
-        return Response(content=buffer.tobytes(), media_type="image/jpeg")
+
+        return Response(
+            content=buffer.tobytes(),
+            media_type="image/jpeg"
+        )
 
     except Exception as e:
-        return JSONResponse(status_code=500, content={"error": str(e)})
+        return JSONResponse(
+            status_code=500,
+            content={"error": str(e)}
+        )
