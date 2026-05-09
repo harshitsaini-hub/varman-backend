2ac902b6676a194f4e6810ccfe5af1e9df965554
harshit saini <harshitsainidkr@gmail.com>
2026-05-09 14:07:31 +0530

    phase 2 completed, initializing phase 3

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse, Response
+from fastapi.responses import JSONResponse
 import cv2
+import uuid
 import numpy as np
 import os
 
 app = FastAPI()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FACE_DIR = os.path.join(BASE_DIR, "faces")
+
+os.makedirs(FACE_DIR, exist_ok=True)
 
 net = cv2.dnn.readNetFromCaffe(
     os.path.join(BASE_DIR, "models", "deploy.prototxt"),
@@ -16,6 +20,7 @@ net = cv2.dnn.readNetFromCaffe(
 @app.post("/detect-face")
 async def detect_face(file: UploadFile = File(...)):
     try:
+
         contents = await file.read()
 
         np_arr = np.frombuffer(contents, np.uint8)
@@ -41,32 +46,50 @@ async def detect_face(file: UploadFile = File(...)):
 
         detections = net.forward()
 
+        faces = []
+
         for i in range(detections.shape[2]):
 
-            confidence = detections[0, 0, i, 2]
+            confidence = float(detections[0, 0, i, 2])
 
-            if confidence > 0.6:
+            if confidence > 0.5:
 
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 
                 (x1, y1, x2, y2) = box.astype("int")
+                
+                x1 = max(0, x1)
+                y1 = max(0, y1)
+                x2 = min(w, x2)
+                y2 = min(h, y2)
+                
+                face_crop = img[y1:y2, x1:x2]
 
-                cv2.rectangle(
-                    img,
-                    (x1, y1),
-                    (x2, y2),
-                    (0, 255, 0),
-                    2
-                )
+                filename = f"{uuid.uuid4()}.jpg"
 
-        _, buffer = cv2.imencode(".jpg", img)
+                filepath = os.path.join(FACE_DIR, filename)
 
-        return Response(
-            content=buffer.tobytes(),
-            media_type="image/jpeg"
-        )
+                cv2.imwrite(filepath, face_crop)
+
+                faces.append({
+                    "id": i,
+                    "confidence": round(confidence, 4),
+                    "saved_as": filename,
+                    "box": {
+                        "x1": int(x1),
+                        "y1": int(y1),
+                        "x2": int(x2),
+                        "y2": int(y2)
+                    }
+                })
+
+        return {
+            "face_count": len(faces),
+            "faces": faces
+        }
 
     except Exception as e:
+
         return JSONResponse(
             status_code=500,
             content={"error": str(e)}
